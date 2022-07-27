60bfcd1ff01757ee112ccb9d6f80234409f1c67b
Zachariah Kelley <zachkelley9@gmail.com>
2022-07-27 16:52:54 -0500

    fixed merge issue with customer.java, changed to non object address

--- DZ-Logistics/src/main/java/com/TeamLogic/beans/Customer.java
+++ DZ-Logistics/src/main/java/com/TeamLogic/beans/Customer.java
@@ -21,30 +21,22 @@ public class Customer {
 	@Column(name="name")
 	private String name;
 	
-<<<<<<< HEAD
 	private String address;
-=======
-	// need to map to addresses @OnetoOne
-	//private Address address;
->>>>>>> b740c337935109843ae9bb5ba8e78346207c6108
 
 	public Customer() {
 		super();
 	}
 
-<<<<<<< HEAD
-	public Customer(int id, int name, String address) {
+	public Customer(int id, String name, String address) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.address = address;
-=======
+}
 	public Customer(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
-		//this.address = new Address();
->>>>>>> b740c337935109843ae9bb5ba8e78346207c6108
 	}
 
 	public int getId() {
@@ -63,7 +55,6 @@ public class Customer {
 		this.name = name;
 	}
 
-<<<<<<< HEAD
 	public String getAddress() {
 		return address;
 	}
@@ -71,9 +62,6 @@ public class Customer {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-=======
-
->>>>>>> b740c337935109843ae9bb5ba8e78346207c6108
 
 	@Override
 	public String toString() {
