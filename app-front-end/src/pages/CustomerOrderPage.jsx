de9dc4a7ff0bdec8ff3b066f1c387048ce21909d
Zachariah Kelley <zachkelley9@gmail.com>
2022-08-08 15:54:41 -0500

    styled order page, looked over code finding bugs

--- app-front-end/src/pages/CustomerOrderPage.jsx
+++ app-front-end/src/pages/CustomerOrderPage.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useState, useRef } from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Button, Card } from "react-bootstrap";
 import { Cookies, useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 
 export const CustomerOrderPage = () => {
@@ -10,6 +11,7 @@ export const CustomerOrderPage = () => {
 
     const [packages, setPackages] = useState([]);
     const [cookies, setCookie] = useCookies();
+    const history = useNavigate();
 
     console.log(packages);
 
@@ -37,23 +39,26 @@ export const CustomerOrderPage = () => {
             await axios.post(`http://localhost:8080/packages/newM`,packages)
             .then(res => setPackages(res.data));
         }
+        history('/CustomerPackages')
     }
 
 
     return (
-        <>
-            <h1>Order</h1>
+        <Card style={{width:'70%',marginLeft:'15%', justifyContent:"center"}}>
+            <h1 style={{textAlign:'center'}}>Order</h1>
             <Form >
                 <Row style={{ justifyContent: "center" }}>
-                    <Form.Label style={{ width: "25%", paddingRight: 0, fontSize: "18px" }}>Number of Packages </Form.Label>
-                    <Button onClick={() => {setNumPackages(numPackages-1)}} style={{ width: "25%", backgroundColor: "white", color: "black", border: "3px solid black" }}>-</Button>
-                    <Form.Label style={{ width: "25%", paddingRight: 0, fontSize: "18px" }}>{numPackages}</Form.Label>
-                    <Button onClick={() => {setNumPackages(numPackages+1)}} style={{ width: "25%", backgroundColor: "white", color: "black", border: "3px solid black" }}>+</Button>
+                    <Form.Label style={{ width: "20%", paddingRight: 0, fontSize: "18px" }}>Number of Packages:</Form.Label>
+                    <Button onClick={() => {numPackages>1?setNumPackages(numPackages-5):setNumPackages(numPackages)}} style={{ width: "4%",height:'5vh', backgroundColor: "white", color: "black", border: "3px solid black",padding:0 }}>-5</Button>
+                    <Button onClick={() => {numPackages>1?setNumPackages(numPackages-1):setNumPackages(numPackages)}} style={{ width: "4%",height:'5vh', backgroundColor: "white", color: "black", border: "3px solid black", padding:0 }}>-</Button>
+                    <Form.Label style={{ width: "5%", paddingRight: 0, fontSize: "18px" }}>{numPackages}</Form.Label>
+                    <Button onClick={() => {setNumPackages(numPackages+1)}} style={{ width: "4%",height:'5vh', backgroundColor: "white", color: "black", border: "3px solid black",padding:0 }}>+</Button>
+                    <Button onClick={() => {setNumPackages(numPackages+5)}} style={{ width: "4%",height:'5vh', backgroundColor: "white", color: "black", border: "3px solid black",padding:0 }}>+5</Button>
                 </Row>
                 <Row style={{ justifyContent: "center" }}>
                     <Button onClick={() => {placeOrder()}} style={{ width: "25%", backgroundColor: "white", color: "black", border: "3px solid black" }}>Place order</Button>
                 </Row>
             </Form>
-        </>
+        </Card>
     );
 }
