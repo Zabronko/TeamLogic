37f35c65a957851fb5e01ff30465eea302dfc7f4
Daniel Livingston <livingstondw@gmail.com>
2022-08-10 18:02:17 -0500

    working on search function, some issues still on fron end

--- app-front-end/src/pages/CustomerPages/Customers.jsx
+++ app-front-end/src/pages/CustomerPages/Customers.jsx
@@ -1,19 +1,22 @@
-import Card from "react-bootstrap/Card";
+import {Card, Form, Button} from "react-bootstrap";
 import { CustomerComponent } from '../../components/Customers/CustomerComponent';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 export const Customers = () => {
   const [customers, setCustomers] = useState([]);
+
 
   useEffect(() => {
       axios.get('http://localhost:8080/customers')
       .then(res => setCustomers(res.data));
   },[]);
 
+
+
   return (
     <>
-    <Card style={{width: "80%", alignContent:"center", marginLeft:"10%"}}>
+    <Card style={{width: "20%", alignContent:"center", marginLeft:"40%"}}>
     </Card>
     <Card style={{width: "80%", alignContent:"center", marginLeft:"10%", marginBottom:"10%"}}>
       <Card.Body>
