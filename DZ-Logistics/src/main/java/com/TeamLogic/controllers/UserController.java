b9894d038b0092ac0508ad754f7c829555461429
Zachariah Kelley <zachkelley9@gmail.com>
2022-08-10 15:29:15 -0500

    Drivers can now accept a job that has at least 5 packages and is idle, they can also complete the job and give check ins as the perform the Job.

--- DZ-Logistics/src/main/java/com/TeamLogic/controllers/UserController.java
+++ DZ-Logistics/src/main/java/com/TeamLogic/controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.TeamLogic.Services.GenericService;
+import com.TeamLogic.beans.Package;
 import com.TeamLogic.beans.Truck;
 import com.TeamLogic.beans.User;
 import com.TeamLogic.repositories.StatusRepository;
@@ -43,20 +44,28 @@ public class UserController {
 	
 
 	@PostMapping("/Driver/truck/{id}")
-	public ResponseEntity<?> DriverSaveTruck(@PathVariable int id, @RequestParam String username) {
+	public ResponseEntity<?> DriverSaveTruck(@PathVariable int id, @RequestParam String username, @RequestParam int statusId) {
 		User user = repo.findById(username).get();
 		Truck truck = truckRepository.findById(id).get();
-		truck.setStatus(statusRepository.findById(2).get());
-		user.setTruck(truck);
+		truck.setStatus(statusRepository.findById(statusId).get());
+		if(truck.getStatus().getId() == 2) {
+			user.setTruck(truck);
+		}else {
+			user.setTruck(null);
+		}
+		for(Package p:truck.getPackages()) {
+			p.setStatus(truck.getStatus());
+		}
+		truckRepository.save(truck);
 		repo.save(user);
 		user.setPassword(null);
 		return ResponseEntity.ok(user);
 	}
 	
 	@GetMapping("/Driver/{username}")
-	public Truck findTruck(@PathVariable String username) {
+	public ResponseEntity<?> findTruck(@PathVariable String username) {
 		User user = repo.findById(username).get();
-		return user.getTruck();
+		return ResponseEntity.ok(user.getTruck());
 	}
 
 	@PostMapping("/apply")
--- app-front-end/src/components/Driver/CurrentJob.jsx
+++ app-front-end/src/components/Driver/CurrentJob.jsx
@@ -1,44 +1,99 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import { Truck } from "../Trucks/Truck";
 
 
 export const CurrentJob = () => {
 
-    const [truck, setTruck] = useState([]);
-    const [cookies, setCookie] = useCookies();
+    const [truck, setTruck] = useState();
+    const [cookies, setCookie, removeCookie] = useCookies();
+    const history = useNavigate();
+    const [checkingIn, setCheckingIn] = useState(false);
+    const checkInTextRef = useRef('');
 
+    const CheckInLabelRef = useRef()
+    const CheckInInputRef = useRef()
     useEffect(() => {
         axios.get(`http://localhost:8080/Driver/${cookies['username']}`)
-        .then(res => setTruck(res.data))
+        .then(res => {
+            setTruck(res.data);
+        })
+
+        if(truck !== undefined & truck !== '') {
+            CheckInLabelRef.current = <h6>{truck.packages[0].lastCheckIn}</h6>;
+            CheckInInputRef.current = <input ref={checkInTextRef.current} placeholder={truck.lastCheckIn}></input>;
+        }
+
     },[])
 
-    if(truck !== undefined) {
+    const handleCheckIn = (value) => {
+        truck.packages.forEach((pack) => {
+            console.log(CheckInInputRef.current)
+            pack.lastCheckIn = checkInTextRef.current;
+        })
+        truck.warehouse = null;
+        axios.put(`http://localhost:8080/packages/Driver`,truck.packages)
+        .then(res => truck.packages = res.data)
+    }
+
+    const handleDriverCompleteJob = () => {
+        truck.status.id = 5
+        axios.post(`http://localhost:8080/Driver/truck/${truck.id}?username=${cookies['username']}&statusId=${truck.status.id}`)
+        .then(res => console.log(res.data))
+        removeCookie('JobWarehouse')
+        window.location.href = 'http://localhost:3000'
+    }
+
+    if(truck !== undefined & truck !== '') {
         return (
             <Card>
                 <Row>
                     <h1>Current Job</h1>
                 </Row>
-                <Row>
+                <Row className="CurrentJobInfo">
                     <Col>
-                        {truck.id}
+                        <h5>Type</h5>
+                        <h6>{truck.type}</h6>
+                    </Col>
+                    <Col>
+                        <h5>Package Amount</h5>
+                        <h6>{truck.packages.length}</h6>
+                    </Col>
+                    <Col>
+                        <h5>From:</h5>
+                        <h6>{cookies['JobWarehouse']}</h6>
+                    </Col>
+                    <Col>
+                        <h5>To:</h5>
+                        {
+                            `${truck.packages[0].customer.city},${truck.packages[0].customer.state}` === cookies['JobWarehouse']?
+                            <h6>Customers of: {truck.packages[0].customer.city},{truck.packages[0].customer.state}</h6>:
+                            <h6>Warehouse In: {truck.packages[0].customer.city},{truck.packages[0].customer.state}</h6>
+                        }
+                    </Col>
+                    <Col>
+                        <h5>Last Check In</h5>
+                        {checkingIn?<input onChange={(e) => {checkInTextRef.current = e.target.value}} placeholder={truck.lastCheckIn}></input>:<h6>{truck.packages[0].lastCheckIn}</h6>}
+                    </Col>
+                    <Col>
+                        {checkingIn?<Button onClick={() => {setCheckingIn(false); handleCheckIn()}}>Save</Button>:<Button onClick={() => {setCheckingIn(true)}} >Check In</Button>}
+                        <Button onClick={() => {handleDriverCompleteJob()}}>Complete Job</Button>
                     </Col>
                 </Row>
             </Card>
         );
     } else {
         return (
-            <Card>
+            <Card style={{justifyContent:'center'}}>
                 <Row>
-                    <h1>Current Job</h1>
+                    <h1 style={{textAlign:'center'}}>Current Job</h1>
                 </Row>
                 <Row>
-                    <Col>
-                        <p>No Current Job</p>
-                        <a href="http://localhost:3000/DriverPortal/GetAJob">Get A Job</a>
-                    </Col>
+                    <h4 style={{textAlign:'center'}}>No Current Job</h4>
+                    <Button onClick={() => {history('/Driver/GetAJob')}} style={{width:'20%', marginLeft:'40%'}} >Get A Job</Button>
                 </Row>
             </Card>
         );
