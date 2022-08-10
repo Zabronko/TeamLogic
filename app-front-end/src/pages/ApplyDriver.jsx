e514f6e9e6a8d7a2c75025a900b9e04a68a40b9a
Daniel Livingston <livingstondw@gmail.com>
2022-08-10 12:43:35 -0500

    added card header for apply driver

--- app-front-end/src/pages/ApplyDriver.jsx
+++ app-front-end/src/pages/ApplyDriver.jsx
@@ -5,6 +5,7 @@ import { Card } from 'react-bootstrap';
 export const ApplyDriver = () => {
   return (
     <Card style={{ width: "30%", marginLeft: "35%", justifyContent: "center" }}>
+      <Card.Header> Register Username an Password to Apply</Card.Header>
     <Card.Body >
       <AddDriverUser />
     </Card.Body>
